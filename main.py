a362cf17be2dae495f7f5c6d5426ec1bd84c7e22
Mohamed El Ousrouti <melousrouti5@gmail.com>
2024-06-28 17:52:31 -0400

    i hope this works

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # from .fade_in_video import fade_in_video
 
@@ -12,8 +13,13 @@ def read_root():
     return {"Hello": "World"}
 
 
+
+class FadeInBody(BaseModel):
+    input_video_path: str
+    fade_duration: float
+
 @app.post("/fade-in-video")
-def read_item(input_video_path: str, fade_duration: float):
+def read_item(FadeInBody):
     # output_file = fade_in_video(input_video_path, "bakwas", fade_duration)
     # Upload to S3 and return URL.
     return {"video": "THIS IS THE EC@ TALKING"}
