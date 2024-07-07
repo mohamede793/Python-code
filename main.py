4c56b61bbda5ddf095063b63e55690a9a6ea0f39
Mohamed El Ousrouti <melousrouti5@gmail.com>
2024-07-06 22:42:22 -0400

    remove body

--- main.py
+++ main.py
@@ -18,67 +18,57 @@ def read_root():
 class Variables(BaseModel):
     variables: Dict[str, Any]
 
-@app.post("/fade-in-video")
-def read_item(body: Variables):
+@app.post("/trim_video")
+def trim_video_endpoint():
+    # output_file = trim_video(body.variables)
+    return "THIS IS THE trim video"
+
+@app.post("/add_video")
+def add_video_endpoint(body: Variables):
+    # output_file = add_video(body.variables)
+    return "THIS IS THE add video"
+
+@app.post("/add_audio")
+def add_audio_endpoint(body: Variables):
+    # output_file = add_audio(body.variables)
+    return "THIS IS THE add audio"
+
+@app.post("/fade_in_video")
+def fade_in_video_endpoint(body: Variables):
     # output_file = fade_in_video(body.variables)
     return "THIS IS THE fade in video"
 
-@app.post("/resize_video")
-def read_item(body: Variables):
-    # output_file = resize_video(body.variables)
-    return "THIS IS THE resize video" #output_file
+@app.post("/fade_out_video")
+def fade_out_video_endpoint(body: Variables):
+    # output_file = fade_out_video(body.variables)
+    return "THIS IS THE fade out video"
 
-@app.post("/trim_video")
-def read_item(body: Variables):
-    # output_file = trim_video(body.variables)
-    return "THIS IS THE trim video" #output_file
+@app.post("/fade_in_audio")
+def fade_in_audio_endpoint(body: Variables):
+    # output_file = fade_in_audio(body.variables)
+    return "THIS IS THE fade in audio"
 
-# @app.post("/add_video")
-# def read_item(body: Variables):
-#     # output_file = add_video(body.variables)
-#     return "THIS IS THE add video"
+@app.post("/fade_out_audio")
+def fade_out_audio_endpoint(body: Variables):
+    # output_file = fade_out_audio(body.variables)
+    return "THIS IS THE fade out audio"
 
-# @app.post("/add_audio")
-# def read_item(body: Variables):
-#     # output_file = add_audio(body.variables)
-#     return "THIS IS THE add audio"
+@app.post("/change_audio_volume")
+def change_audio_volume_endpoint(body: Variables):
+    # output_file = change_audio_volume(body.variables)
+    return "THIS IS THE change audio volume"
 
-# @app.post("/fade_in_video")
-# def read_item(body: Variables):
-#     # output_file = fade_in_video(body.variables)
-#     return "THIS IS THE fade in video"
+@app.post("/remove_audio")
+def remove_audio_endpoint(body: Variables):
+    # output_file = remove_audio(body.variables)
+    return "THIS IS THE remove audio"
 
-# @app.post("/fade_out_video")
-# def read_item(body: Variables):
-#     # output_file = fade_out_video(body.variables)
-#     return "THIS IS THE fade out video"
+@app.post("/other_edit")
+def other_edit_endpoint(body: Variables):
+    # output_file = other_edit(body.variables)
+    return "THIS IS THE other edit"
 
-# @app.post("/fade_in_audio")
-# def read_item(body: Variables):
-#     # output_file = fade_in_audio(body.variables)
-#     return "THIS IS THE fade in audio"
-
-# @app.post("/fade_out_audio")
-# def read_item(body: Variables):
-#     # output_file = fade_out_audio(body.variables)
-#     return "THIS IS THE fade out audio"
-
-# @app.post("/change_audio_volume")
-# def read_item(body: Variables):
-#     # output_file = change_audio_volume(body.variables)
-#     return "THIS IS THE change audio volume"
-
-# @app.post("/remove_audio")
-# def read_item(body: Variables):
-#     # output_file = remove_audio(body.variables)
-#     return "THIS IS THE remove audio"
-
-# @app.post("/other_edit")
-# def read_item(body: Variables):
-#     # output_file = other_edit(body.variables)
-#     return "THIS IS THE other edit"
-
-# @app.post("/help")
-# def read_item(body: Variables):
-#     # output_file = help(body.variables)
-#     return "THIS IS THE help"
+@app.post("/help")
+def help_endpoint(body: Variables):
+    # output_file = help(body.variables)
+    return "THIS IS THE help"
